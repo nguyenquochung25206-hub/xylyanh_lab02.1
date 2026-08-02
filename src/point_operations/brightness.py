3fd98449d8d1941b65e4e86df92253876f7e02f0
xuanmai <maintx1770@ut.edu.vn>
2026-08-02 13:22:59 +0700

    Update brightness.py

--- src/point_operations/brightness.py
+++ src/point_operations/brightness.py
@@ -1,1 +1,24 @@
+import cv2
+import numpy as np
 
+# Đọc ảnh
+img = cv2.imread("anh_input3.jpg")
+
+# Tăng độ sáng
+value = 50
+bright = cv2.convertScaleAbs(img, alpha=1.0, beta=50)
+
+# Giảm độ sáng
+dark = cv2.convertScaleAbs(img, alpha=1.0, beta=-30)
+
+# Hiển thị
+cv2.imshow("Original", img)
+cv2.imshow("Bright", bright)
+cv2.imshow("Dark", dark)
+
+cv2.imwrite("anh_sang.png", bright)
+cv2.imwrite("anh_toi.png", dark)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
